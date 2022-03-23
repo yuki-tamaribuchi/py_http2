11c6302e25d2f39a80c67f89a737a720cafe5dfe
TAMARIBUCHI Yuki <uk.tmbc2408@gmail.com>
2022-03-24 07:49:31 +0900

    Move upgrade flow to switch_protocol function

--- src/http2/server/worker.py
+++ src/http2/server/worker.py
@@ -15,34 +15,52 @@ def send_response(client_sock, response):
 	return True
 
 
+def switch_protocol(client_sock, request):
+	if not "Upgrade" in request.options and not request.options["Upgrade"] == "h2c":
+		print("h2c error")
+		raise Exception
+
+	if not "HTTP2-Settings" in request.options:
+		print("Request has no HTTP2-Settings filed")
+		raise Exception
+
+	upgrade_options_dict = {
+		"Connection": "Upgrade",
+		"Upgrade": "h2c"
+	}
+	response = Response("HTTP/1.1", 101, upgrade_options_dict)
+
+	if not send_response(client_sock, response.get_raw_response()):
+		print("Respone send error")
+		raise Exception
+
+	return True
+
+
 class Worker(Thread):
 	def __init__(self, client_sock):
 		self.client_sock = client_sock
 	
 	def start(self):
 		raw_request = recv_request(self.client_sock)
+		if b"PRI * HTTP/2.0\r\n\r\nSM\r\n\r\n" in raw_request:
+			print("HTTP/2.0 prior knowledge is not supported now")
+			raise Exception
+		
 		request = Request(raw_request)
 
 
-
-		#Upgrade connection to HTTP/2.0
-		if not "Upgrade" in request.options and not request.options["Upgrade"] == "h2c":
-			print("h2c error")
-			raise Exception
-
-		if not "HTTP2-Settings" in request.options:
-			print("Request has no HTTP2-Settings filed")
-			raise Exception
-
-		upgrade_options_dict = {
-			"Connection": "Upgrade",
-			"Upgrade": "h2c"
-		}
-		response = Response("HTTP/1.1", 101, upgrade_options_dict)
-		if not send_response(self.client_sock, response.get_raw_response()):
-			print("Respone send error")
-			raise Exception
-
+		#Switch Protocol if request http version 1.1
+		if request.version == "HTTP/1.1":
+			if "Upgrade" in request.options:
+				if not switch_protocol(self.client_sock, request):
+					print("Switching protocol error")
+					raise Exception
+			else:
+				send_response(self.client_sock, b"HTTP/1.1 505 HTTP Version Not Supported\r\n\r\n")
+				return
+				
+		
 
 		#Recieve preface
 		raw_request = recv_request(self.client_sock)
