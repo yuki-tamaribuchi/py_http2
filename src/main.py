baf52b7526b767dab9c35d699a3976a2047f082b
TAMARIBUCHI Yuki <uk.tmbc2408@gmail.com>
2022-04-12 08:55:37 +0900

    Create sample_app

--- src/main.py
+++ src/main.py
@@ -1,11 +1,17 @@
 from http2.server import Server
 
 
+def sample_app(request):
+	print("app recieved request")
+	print(request)
+	return
+
+
 if __name__ == "__main__":
 	try:
-		server = Server("127.0.0.1", 8000)
+		server = Server("127.0.0.1", 8000, sample_app)
 		print("Server was started at http://127.0.0.1:8000")
 		server.serve()
 
 	except KeyboardInterrupt:
-		pass
+		server.stop()
