1d1c0755271dc04effe70a7fe1a9a964bd305f9c
TAMARIBUCHI Yuki <uk.tmbc2408@gmail.com>
2022-04-17 09:24:06 +0900

    Create Data class

---
+++ src/http2/frame/data.py
@@ -0,0 +1,28 @@
+from ..utils.flags import is_flagged
+
+class Data:
+	def __init__(self, data, padding_length, is_end_stream=False):
+		assert(type(data) == bytes)
+		self.data = data
+		self.padding_length = padding_length
+		self.is_end_stream = is_end_stream
+
+	
+	@classmethod
+	def load_raw_frame(self, raw_frame, flags):
+		if is_flagged(flags, 0):
+			is_end_stream = True
+		else:
+			is_end_stream = False
+
+		if is_flagged(flags, 3):
+			padding_length = raw_frame[0]
+			raw_frame = raw_frame[1:padding_length]
+		else:
+			padding_length = 0
+
+		return Data(
+			data=raw_frame,
+			padding_length=padding_length,
+			is_end_stream=is_end_stream
+		)
