04f547e1308e75ec21f93f5fe8abc2f5fa095cfc
TAMARIBUCHI Yuki <uk.tmbc2408@gmail.com>
2022-04-20 00:04:32 +0900

    Add load_raw_frame method and modify init

--- src/http2/frame/settings.py
+++ src/http2/frame/settings.py
@@ -4,15 +4,26 @@ class Settings:
 		"2": "SETTINGS_ENABLE_PUSH",
 		"3": "SETTINGS_MAX_CONCURRENT_STREAMS", 
 		"4": "SETTINGS_INITIAL_WINDOW_SIZE",
-		"5": "SETTINGS_INITIAL_WINDOW_SIZE",
+		"5": "SETTINGS_MAX_WINDOW_SIZE",
 		"6": "SETTINGS_MAX_HEADER_LIST_SIZE"
 	}
 
-	def __init__(self, payload):
-		self.identifier = int.from_bytes(payload[0:2], "big")
+	def __init__(self, identifier, value):
+		self.identifier = identifier
 		self.identifier_str = self.IDENTIFIERS[str(self.identifier)]
-		self.value = int.from_bytes(payload[2:], "big")
+		self.value = value
 	
+
+	@classmethod
+	def load_raw_frame(self, raw_frame):
+		frame_list = []
+
+		for i in range(0, len(raw_frame), 6):
+			identifier = int.from_bytes(raw_frame[0:2], "big")
+			value = int.from_bytes(raw_frame[2:], "big")
+			frame = Settings(identifier, value)
+			frame_list.append(frame)
+		return frame_list
 
 	def get_raw_frame(self):
 		import struct
