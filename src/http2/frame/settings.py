deb8fb8f3f258ef42a3417d24c5af3d434a70dd8
TAMARIBUCHI Yuki <uk.tmbc2408@gmail.com>
2022-04-21 07:56:41 +0900

    Create SettingField class and modify settings class

--- src/http2/frame/settings.py
+++ src/http2/frame/settings.py
@@ -1,4 +1,6 @@
-class Settings:
+from ..utils.flags import is_flagged
+
+class SettingField:
 	IDENTIFIERS = {
 		"1": "SETTINGS_HEADER_TABLE_SIZE",
 		"2": "SETTINGS_ENABLE_PUSH",
@@ -7,37 +9,60 @@ class Settings:
 		"5": "SETTINGS_MAX_WINDOW_SIZE",
 		"6": "SETTINGS_MAX_HEADER_LIST_SIZE"
 	}
-
+	
 	def __init__(self, identifier, value):
 		self.identifier = identifier
-		self.identifier_str = self.IDENTIFIERS[str(self.identifier)]
 		self.value = value
+
+	def __str__(self):
+		return "%s(%d): %s"%(
+			self.IDENTIFIERS[str(self.identifier)],
+			self.identifier,
+			str(self.value)
+		)
+
+
+
+class Settings:
+	def __init__(self, fields, is_ack=False):
+		if fields and is_ack:
+			print("Do not set fields in acknowledge setting frame")
+		self.fields = fields
+		self.is_ack = is_ack
 	
 
 	@classmethod
-	def load_raw_frame(self, raw_frame):
-		frame_list = []
+	def load_raw_frame(self, raw_frame, flags):
+		if is_flagged(flags, 0b1):
+			is_ack = True
+		else:
+			is_ack = False
 
+		fields = []
 		for i in range(0, len(raw_frame), 6):
 			identifier = int.from_bytes(raw_frame[0:2], "big")
 			value = int.from_bytes(raw_frame[2:], "big")
-			frame = Settings(identifier, value)
-			frame_list.append(frame)
-		return frame_list
+			setting_field = SettingField(identifier, value)
+			fields.append(setting_field)
+			
+		return Settings(fields, is_ack)
 
 	def get_raw_frame(self):
 		import struct
-		raw_frame = struct.pack(
-			"!HI",
-			self.identifier,
-			self.value
-		)
+		raw_frame = b""
+
+		if self.fields:
+			for field in self.fields:
+				raw_frame += struct.pack(
+					"!HI",
+					field.identifier,
+					field.value
+				)
+
 		return raw_frame
 	
 
 	def __repr__(self):
-		return "%s(%d): %d"%(
-			self.identifier_str,
-			self.identifier,
-			self.value
+		return "Settings frame has %s fields"%(
+			len(self.fields)
 		)
