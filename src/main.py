63c07d1e7f36e4846461686a5527d52b5d17d922
TAMARIBUCHI Yuki <uk.tmbc2408@gmail.com>
2022-04-22 08:01:50 +0900

    Update

--- src/main.py
+++ src/main.py
@@ -1,10 +1,25 @@
 from http2.server import Server
 
+from http2.response import H2Response
+
 
 def sample_app(request):
+	print("***************************************************************************")
 	print("app recieved request")
 	print(request)
-	return
+	print("***************************************************************************")
+	options = {
+		"Accept-Encoding": "gzip, deflate",
+		"Accept-Language": "ja",
+		"Test-Field": "test"
+	}
+	if request.body:
+		request_body = request.body
+	else:
+		request_body = b""
+	response_body = b"Requested data: %s"%(request_body)
+	response = H2Response(200, options, response_body)
+	return response
 
 
 if __name__ == "__main__":
