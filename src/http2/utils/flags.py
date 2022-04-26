b7e7ca62439719e33435e33e519a01fe079ba25b
TAMARIBUCHI Yuki <uk.tmbc2408@gmail.com>
2022-04-27 07:54:35 +0900

    Create set_flag function

--- src/http2/utils/flags.py
+++ src/http2/utils/flags.py
@@ -3,3 +3,10 @@ def is_flagged(flags, flag_n):
 	if flags & flag_n == flag_n:
 		return True
 	return False
+
+
+def set_flag(flags, flag_n):
+	assert(flag_n >= 0b00000000 and flag_n<=0b11111111)
+
+	flags = flags | flag_n
+	return flags
